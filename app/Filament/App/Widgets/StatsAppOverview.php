c671487ce1da48f86b3d812dcba76f2d58a82757
ariany-f <ariany_f@hotmail.com>
2024-08-18 23:15:16 -0300

    Ajuste app não vê teams

--- app/Filament/App/Widgets/StatsAppOverview.php
+++ app/Filament/App/Widgets/StatsAppOverview.php
@@ -21,11 +21,6 @@ class StatsAppOverview extends BaseWidget
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
-            Stat::make('Departments', Department::query()->whereBelongsTo(Filament::getTenant())->count())
-                ->label(ucwords(trans_choice('custom.team.label', 2)))
-                ->description(__('custom.team.all_from_database'))
-                ->descriptionIcon('heroicon-m-arrow-trending-down')
-                ->color('danger'),
             Stat::make('Employees', Employee::query()->whereBelongsTo(Filament::getTenant())->count())
                 ->label(ucwords(trans_choice('custom.employee.label', 2)))
                 ->description(__('custom.employee.all_from_database'))
